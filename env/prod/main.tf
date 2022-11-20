0c2074b59aa32749c594bf3aa31f3f0293b73fe1
leosantosw <leonardosantosx0077@gmail.com>
2022-11-20 17:24:33 -0300

    add resource for vpc and sg

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -3,5 +3,7 @@ module "infrastructure" {
 
   aws_region = "us-east-1"
   aws_profile = "leo-aws"
+  vpc_ecs = "vpc-ecs"
+  security_group = "outline-sg"
   ecr_postgres_repository = "outline-postgres"
 }
