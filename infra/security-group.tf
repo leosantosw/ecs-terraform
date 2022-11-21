ded57a5caba5e167d55657b52801e9793048454a
leosantosw <leonardosantosx0077@gmail.com>
2022-11-21 09:42:49 -0300

    fix: open ports in sg

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -4,10 +4,16 @@ resource "aws_security_group" "security_group" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    description = "outline-sg"
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
