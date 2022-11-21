352e772e63096a9bad88fd7e2004d88b6f1fa910
leosantosw <leonardosantosx0077@gmail.com>
2022-11-21 00:00:28 -0300

    fix: enable nat gateway on vpc

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -3,6 +3,7 @@ module "vpc" {
 
   name = var.vpc_ecs
   cidr = "10.0.0.0/16"
+  enable_nat_gateway = true
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
