5939947dc302871821c92061d84dd79fa80227f9
leosantosw <leonardosantosx0077@gmail.com>
2022-11-20 22:37:23 -0300

    feat: add cluster, service and task definition

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -2,8 +2,18 @@ module "infrastructure" {
   source = "../../infra/"
 
   aws_region = "us-east-1"
+  
   aws_profile = "leo-aws"
+
   vpc_ecs = "vpc-ecs"
+  
   security_group = "outline-sg"
+  
   ecr_postgres_repository = "outline-postgres"
+  
+  ecs_cluster_name = "outline-cluster"
+
+  ecs_task_definition = "postgres-outline-task"
+
+  ecs_service_name = "postgres-outline-service"
 }
